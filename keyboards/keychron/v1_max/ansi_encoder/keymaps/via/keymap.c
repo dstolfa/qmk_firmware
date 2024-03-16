d149e019fdd48af513e0c6dc444f3a151837cbb6
Domagoj Stolfa <ds815@cam.ac.uk>
2024-03-16 21:38:49 +0000

    [v1_max] Add copyright.

--- keyboards/keychron/v1_max/ansi_encoder/keymaps/via/keymap.c
+++ keyboards/keychron/v1_max/ansi_encoder/keymaps/via/keymap.c
@@ -1,4 +1,5 @@
 /* Copyright 2023 @ Keychron (https://www.keychron.com)
+ * Copyright 2024 @ Domagoj Stolfa <ds815@gmx.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
